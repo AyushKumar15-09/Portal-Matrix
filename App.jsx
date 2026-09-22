799a43121515128129c89557ea0a7ee47268a4a5
Ayush Kumar <ayushait2004.ait@gmail.com>
2026-09-22 19:41:23 +0530

    Import icons for button functionality

--- App.jsx
+++ App.jsx
@@ -3,3 +3,5 @@
 import React, { useState } from 'react';
 // We bring in Auth0 tools to handle user login safely.
 import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
+// We bring in a few simple icons for our buttons.
+import { LogOut, Menu, Grid3x3, X } from 'lucide-react';
